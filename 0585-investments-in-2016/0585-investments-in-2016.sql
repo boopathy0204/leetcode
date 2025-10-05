afdc685ed23b196afa51c996ebb75f49b13e7c30
boopathy0204 <boopathyp0204@gmail.com>
2025-10-05 16:21:36 +0530

    Time: 453 ms (99.56%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,21 +1,20 @@
-# Write your MySQL query statement below
-WITH tem AS (
-    SELECT tiv_2015, tiv_2016
-FROM Insurance
-WHERE (lat, lon) IN (
-    SELECT lat, lon
-    FROM Insurance
-    GROUP BY lat, lon
-    HAVING COUNT(*) = 1
-)
-)
 SELECT 
-    round(SUM(tiv_2016),2)AS tiv_2016
+    ROUND(SUM(tiv_2016),2) AS tiv_2016
 FROM(
-    SELECT 
-        SUM(tiv_2016)AS tiv_2016
-    FROM
-        tem
-    GROUP BY tiv_2015
-    HAVING COUNT(tiv_2015) > 1
-    ) AS S
+    SELECT
+        SUM(tiv_2016) AS tiv_2016,
+        val
+    FROM(
+        SELECT 
+            COUNT(tiv_2015) OVER(PARTITION BY tiv_2015) AS val,
+            tiv_2015,
+            tiv_2016,
+            lat,
+            lon
+        FROM
+            Insurance
+    ) AS s
+GROUP BY lat ,lon
+HAVING COUNT(tiv_2015) = 1
+) AS SS
+WHERE val > 1
